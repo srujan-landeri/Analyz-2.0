27b140535b62ca29826cfe5098a5416f7b8988b8
Landeri Srujan <66351075+srujan-landeri@users.noreply.github.com>
2024-11-04 19:06:48 +0530

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,8 @@ import polka from 'polka';
 import { URLSearchParams } from 'url';
 
 // Add to your imports
-const GOOGLE_CLIENT_ID = "944125795870-j1vgdubdra1njjjq7t8sumjrn0p1fj3i.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+const GOOGLE_CLIENT_ID = "xxxxx";
+const GOOGLE_CLIENT_SECRET = "xxxxx";
 const REDIRECT_URI = 'http://localhost:54321/callback';
 
 export function activate(context: vscode.ExtensionContext) {
